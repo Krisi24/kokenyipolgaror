acd5a1c97a807523400fd21cc84e8c2cd3888cc5
Cseh Kristóf Péter <h982327@stud.u-szeged.hu>
2023-09-22 19:42:34 +0200

    small changes: content and footer

--- kokenyipolgaror/app/Views/components/footer.php
+++ kokenyipolgaror/app/Views/components/footer.php
@@ -2,9 +2,9 @@
   <img class="logo" src="<?= base_url(); ?>images/logo.webp"/>
   
   <div class="footer-content">
-  Kapcsolat:
-    <img class="email" src="<?= base_url(); ?>images/email_icon.png" alt="">
-      bejelentes@kokenyipolgaror.hu
+    <div>Kapcsolat:</div>
+    <div><img class="email" src="<?= base_url(); ?>images/email_icon.png" alt=""><span class="data" >elnok@kokenyipolgaror.hu</span></div>
+    <div><img class="email" src="<?= base_url(); ?>images/email_icon.png" alt=""><span class="data">bejelentes@kokenyipolgaror.hu</span> (weboldallal kapcsolatban)</div>
   </div>
   
 </footer>
--- kokenyipolgaror/app/Views/public/home.php
+++ kokenyipolgaror/app/Views/public/home.php
@@ -1,6 +1,7 @@
 <?= $this->extend('layouts/main.php'); ?>
 
 <?= $this->section('content'); ?>
+
   <div class="section">
     <div class="title">Some Title</div>
     <div class="text-content">
@@ -10,66 +11,7 @@
       <img src="<?= base_url(); ?>images/example.jpg" />
       <img src="<?= base_url(); ?>images/example.jpg" />
       <img src="<?= base_url(); ?>images/example.jpg" />
-      <img src="<?= base_url(); ?>images/example.jpg" />
-      <img src="<?= base_url(); ?>images/example.jpg" />
-      <img src="<?= base_url(); ?>images/example.jpg" />
-      <img src="<?= base_url(); ?>images/example.jpg" />
-      <img src="<?= base_url(); ?>images/example.jpg" />
     </div>
-  </div>
-
-  <div class="section">
-	  <img src="<?= base_url(); ?>images/flag.png" />
-    im
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    im
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    im
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    im
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    im
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
-    <br>
-    klsdfn sdflfjn lksdfg dlgj lsdfjg lsdfkmbjésldmbsdfélbk dfs
   </div>
   
 <?= $this->endSection(); ?>
